a1360f8dac58361b7e11d0a8bf321119467bc522
Jukka Aho <ahojukka5@gmail.com>
2026-07-26 16:40:34 +0300

    feat: register merge policy enforcement

--- src/weave_jacquard/mcp_build.py
+++ src/weave_jacquard/mcp_build.py
@@ -1,9 +1,10 @@
 """Public Jacquard entry point for the Weave MCP server."""
 
+from weave_frontend import mcp_policy as _mcp_policy
 from weave_frontend import mcp_preflight as _mcp_preflight
 from weave_frontend.mcp_build import main
 
-_ = _mcp_preflight
+_ = (_mcp_policy, _mcp_preflight)
 
 __all__ = ["main"]
 
