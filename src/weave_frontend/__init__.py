a11b7491e702ab6b5bab64e64b473bad9f8e91d1
Jukka Aho <ahojukka5@gmail.com>
2026-07-26 19:44:59 +0300

    refactor: export full race-safe workspace

--- src/weave_frontend/__init__.py
+++ src/weave_frontend/__init__.py
@@ -6,7 +6,7 @@ and existing implementation modules retain compatibility.
 """
 
 from .compiler_bridge import CompilerBridge
-from .concurrent_sexpr import SExpressionWorkspace
+from .concurrent_workspace import SExpressionWorkspace
 from .errors import ConflictError, NotFoundError, ValidationError, WeaveFrontendError
 from .service import MergeResult
 from .source_map import render_with_node_map, smallest_node_for_span
