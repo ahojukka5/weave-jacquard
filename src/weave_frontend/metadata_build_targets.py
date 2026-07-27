22a6c77c641a8f45febee04ac0a7d07e1e26b21f
Jukka Aho <ahojukka5@gmail.com>
2026-07-27 04:24:37 +0300

    style: remove unused metadata import

--- src/weave_frontend/metadata_build_targets.py
+++ src/weave_frontend/metadata_build_targets.py
@@ -2,8 +2,6 @@
 
 from __future__ import annotations
 
-from typing import Any
-
 from .concurrent_build_targets import BuildTargetRegistry as _BaseBuildTargetRegistry
 from .errors import NotFoundError, ValidationError
 from .project_metadata import is_project_metadata_document
