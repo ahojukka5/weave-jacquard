6d4d3f995d9aae7749c232c3dd699c429c635bf5
Jukka Aho <ahojukka5@gmail.com>
2026-07-27 15:32:56 +0300

    expose application composition API

---
+++ src/weave_jacquard/application.py
@@ -0,0 +1,21 @@
+"""Public application-composition API for Jacquard."""
+
+from weave_frontend.application import (
+    APPLICATION_MANIFEST_FORMAT,
+    PUBLIC_CONFIGURATION_VARIABLES,
+    TOOL_MANIFEST_FORMAT,
+    ApplicationCompositionError,
+    JacquardApp,
+    build_tool_manifest,
+    registered_tool_names,
+)
+
+__all__ = [
+    "APPLICATION_MANIFEST_FORMAT",
+    "PUBLIC_CONFIGURATION_VARIABLES",
+    "TOOL_MANIFEST_FORMAT",
+    "ApplicationCompositionError",
+    "JacquardApp",
+    "build_tool_manifest",
+    "registered_tool_names",
+]
